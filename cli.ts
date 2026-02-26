e2ce5832b274aa79d4a96be021ebcd91ae2abb26
Shalini Saloni <shalinisaloni2801@gmail.com>
2026-02-26 10:32:07 +0530

    Add calculator

--- cli.ts
+++ cli.ts
@@ -8,4 +8,24 @@ program
 .command("greet <name>")
 .action((name) => {console.log(`Hello, ${name}!`)});
 
+program 
+.command("add <n1> <n2>")
+.description("Adds two numbers")
+.action((n1, n2) => {console.log(Number(n1) + Number(n2))});
+
+program
+.command("subtract <n1> <n2>")
+.description("Subtracts second number from first")
+.action((n1, n2) => {console.log(Number(n1) - Number(n2))});
+
+program
+.command("multiply <n1> <n2>")
+.description("Multiplies two numbers")
+.action((n1, n2) => {console.log(Number(n1) * Number(n2))});
+
+program 
+.command("divide <n1> <n2>")
+.description("Divides first number by second")
+.action((n1, n2) => {console.log(Number(n1) / Number(n2))});
+
 program.parse();
